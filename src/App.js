3bef6ba4dd29858b450a68c982657a7242a61995
NickBwalley <nickbwalley@gmail.com>
2023-03-29 15:37:08 +0300

    globalstate components

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
 import AddTransaction from "./components/AddTransaction";
 
+import { GlobalProvider } from "./context/GlobalState";
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -17,7 +18,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import Transaction from "./Transaction";
+import { GlobalContext } from "../context/GlobalState";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        className
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
   );
