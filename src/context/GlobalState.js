262056c58132599abe66b228551e573e4288add9
NickBwalley <nickbwalley@gmail.com>
2023-03-29 16:03:53 +0300

    completed functionality

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,12 +2,7 @@ import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 // initial state
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-  ],
+  transactions: [],
 };
 
 // Create context
@@ -24,11 +19,20 @@ export const GlobalProvider = ({ children }) => {
       payload: id,
     });
   }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
         deleteTransaction,
+        addTransaction,
       }}
     >
       {children}
