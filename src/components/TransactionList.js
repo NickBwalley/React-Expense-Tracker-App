9061dd24d6b08f675fc4fcd6c4702a7d5fd573ed
NickBwalley <nickbwalley@gmail.com>
2023-03-29 15:14:49 +0300

    components initialization

---
+++ src/components/TransactionList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TransactionList = () => {
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        className
+        <li className="minus">
+          Cash <span>-$400</span>
+          <button className="delete-btn">x</button>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default TransactionList;
